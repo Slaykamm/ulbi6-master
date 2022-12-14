6ed70b78545766133801c1e17398b3baf026938f
Slay <destpoch@mail.ru>
2022-12-14 21:26:04 +0300

    stories

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -14,6 +14,10 @@ export default ({ config }: { config: webpack.Configuration }) => {
     };
     config!.resolve!.modules!.push(paths.src);
     config!.resolve!.extensions!.push('.ts', '.tsx');
+    config!.resolve!.alias = {
+        ...config!.resolve!.alias,
+        '@': paths.src,
+    };
 
     // eslint-disable-next-line no-param-reassign
     // @ts-ignore
@@ -36,7 +40,7 @@ export default ({ config }: { config: webpack.Configuration }) => {
             __IS_DEV__: JSON.stringify(true),
             __API__: JSON.stringify('http://testapi.ru/'),
             __PROJECT__: JSON.stringify('storybook'),
-        })
+        }),
     );
 
     return config;
