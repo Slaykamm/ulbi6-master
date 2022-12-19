24a5cf992a65bc0b1a3f77b1c303345929dd416c
Slay <destpoch@mail.ru>
2022-12-19 21:20:50 +0300

    added AppImageFallback

--- src/entitles/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entitles/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -17,6 +17,8 @@ import {
 import { ArticleView } from '../../model/consts/articleConsts';
 import { ArticleTextComponent } from '../ArticleImageTextComponent/ui/ArticleTextComponent';
 import { getRouteArticleDetails } from '@/shared/const/router';
+import { AppImage } from '@/shared/ui/AppImage';
+import { Skeleton } from '@/shared/ui/Skeleton';
 
 interface ArticleListItemProps {
     className?: string;
@@ -57,7 +59,12 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
                     </div>
                     <Text title={article.title} className={cls.title} />
                     {types}
-                    <img src={article.img} alt={article.title} className={cls.img} />
+                    <AppImage
+                        fallBack={<Skeleton width={200} height={200} />}
+                        src={article.img}
+                        alt={article.title}
+                        className={cls.img}
+                    />
                     {textBlock && (
                         <ArticleTextComponent block={textBlock} className={cls.textBlock} />
                     )}
@@ -83,7 +90,12 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
         >
             <Card className={cls.card}>
                 <div className={cls.imageWrapper}>
-                    <img src={article.img} className={cls.img} alt={article.title} />
+                    <AppImage
+                        fallBack={<Skeleton width="100%" height={250} />}
+                        src={article.img}
+                        className={cls.img}
+                        alt={article.title}
+                    />
                     <Text text={article.createAt} className={cls.date} />
                 </div>
                 <div className={cls.infoWrapper}>
--- src/shared/ui/Avatar/Avatar.tsx
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -2,12 +2,17 @@ import { useTranslation } from 'react-i18next';
 import { CSSProperties, useMemo } from 'react';
 import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import cls from './Avatar.module.scss';
+import { AppImage } from '../AppImage';
+import UserIcon from '../../assets/icons/user-filled.svg';
+import { Icon } from '../Icon';
+import { Skeleton } from '../Skeleton';
 
 interface AvatarProps {
-   className?: string;
-   src?: string;
-   size?: number;
-   alt?: string;
+    className?: string;
+    src?: string;
+    size?: number;
+    alt?: string;
+    fallbackInverted?: boolean;
 
 }
 
@@ -15,20 +20,27 @@ export const Avatar = (props: AvatarProps) => {
     const {
         className,
         src,
-        size,
+        size = 100,
         alt,
+        fallbackInverted,
     } = props;
 
     const { t } = useTranslation();
 
     const styles = useMemo<CSSProperties>(() => ({
-        width: size || 100,
-        height: size || 100,
+        width: size,
+        height: size,
     }), [size]);
 
     const mods: Mods = {};
+
+    const fallback = <Skeleton width={size} height={size} border="50%" />;
+    const errorFallback = <Icon inverted={fallbackInverted} width={size} height={size} Svg={UserIcon} />;
+
     return (
-        <img
+        <AppImage
+            fallBack={fallback}
+            errorFallback={errorFallback}
             src={src}
             style={styles}
             className={classNames(cls.Avatar, mods, [className])}
