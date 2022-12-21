70fe6c67ffd20bab4a48136da07fdf969eec0f2a
Slay <destpoch@mail.ru>
2022-12-21 20:41:42 +0300

    unit test cont

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -48,7 +48,7 @@ export default {
             pageTitle: 'Test Report',
             publicPath: '<rootDir>/reports/unit',
             filename: 'report.html',
-            openReport: true,
+            // openReport: true,
             inlineSource: true,
         }],
     ],
