e6992f737cfb2cf421f8b64bf5725eafe162069c
Slay <destpoch@mail.ru>
2022-12-25 17:15:26 +0300

    added Cypress e2e

---
+++ cypress/e2e/common/routing.cy.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable indent */
+import { selectByTestId } from '../../helpers/selectByTestId';
+
+describe('Роутинг', () => {
+  describe('Пользователь не авторизован', () => {
+    it('Главная страница', () => {
+      cy.visit('/');
+      cy.get(selectByTestId('MainPage')).should('exist');
+    });
+    it('Страницу профиля', () => {
+      cy.visit('/profile/1');
+      cy.get(selectByTestId('MainPage')).should('exist');
+    });
+    it('Страницу профиля', () => {
+      cy.visit('/psdfsdfsdf');
+      cy.get(selectByTestId('NotFoundPage')).should('exist');
+    });
+  });
+
+  describe('Пользователь  авторизован', () => {
+    beforeEach(() => {
+      cy.login();
+    });
+    it('Переход открывает страницу профиля', () => {
+      cy.visit('/profile/1');
+      cy.get(selectByTestId('ProfilePage')).should('exist');
+    });
+    it('Переход на страницу со списком статей', () => {
+      cy.visit('/articles');
+      cy.get(selectByTestId('ArticlesPage')).should('exist');
+    });
+  });
+});
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -29,6 +29,7 @@ const ArticlesPage = (props: ArticlesPageProps) => {
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount={false}>
             <Page
+                data-testid="ArticlesPage"
                 onScrollEnd={onLoadNextPart}
                 className={classNames(cls.ArticlesPage, {}, [className])}
             >
